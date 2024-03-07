2091c33c88fd9ff1a46482dfb9acb28323ec424c
leonardo.salles <leonardocsalles05@gmail.com>
2024-03-07 14:39:52 -0300

    third-version

--- app/src/main/java/com/example/juros/ResultadoJuros.kt
+++ app/src/main/java/com/example/juros/ResultadoJuros.kt
@@ -2,18 +2,18 @@ package com.example.juros
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.widget.TextView
 
 class ResultadoJuros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado_juros)
         setSupportActionBar(findViewById(R.id.toolbar))
-        val montante = findViewById<EditText>(R.id.Montante)
-        val juros = findViewById<EditText>(R.id.TotalJuros)
-        val parcela = findViewById<EditText>(R.id.Parcela)
+        val montante = findViewById<TextView>(R.id.Montante)
+        val juros = findViewById<TextView>(R.id.TotalJuros)
+        val parcela = findViewById<TextView>(R.id.Parcela)
         val valormont = intent.getDoubleExtra("calcmont", 0.0)
-        val valortotal = intent.getDoubleExtra("calcvalor", 0.0)
+        val valortotal = intent.getDoubleExtra("calcjuros", 0.0)
         val valorparcela = intent.getDoubleExtra("calcparcela", 0.0)
         montante.text = valormont.toString()
         juros.text = valortotal.toString()
